f84e4fa47bcd2dab3d65066346f754b6a9a3ea2f
M4alik <mmalik.amara@gmail.com>
2022-07-21 16:29:03 +0200

    formulaire inscription

--- migrations/Version20220721113653.php
+++ migrations/Version20220721113653.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220720120247 extends AbstractMigration
+final class Version20220721113653 extends AbstractMigration
 {
     public function getDescription(): string
     {
